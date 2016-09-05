970f1d86970b17b8add7f9eef82704e5fb2787c5
William Pettersson <william@ewpettersson.se>
2016-09-06 09:41:51 +1000

    Fix typo in variable name

--- 2var.c
+++ 2var.c
@@ -5,8 +5,8 @@ int main(int argc, char** argv) {
   for(int i = 0; i < 10; ++i)
     printf("%d ",i);
 
-  for(int j = 15; i < 20; ++i)
-    printf("%d ",i);
+  for(int j = 15; j < 20; ++j)
+    printf("%d ",j);
 
   printf("\n");
   return 0;
